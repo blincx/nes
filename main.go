7edc736886c4e739b321e24fbc1303e8c4c6d684
Eric Blincow <eblincow@barracuda.com>
2019-12-07 05:12:34 -0800

    first commit

--- main.go
+++ main.go
@@ -6,16 +6,19 @@ import (
 	"os"
 	"path"
 	"strings"
-
+	"fmt"
 	"github.com/fogleman/nes/ui"
 )
 
 func main() {
 	log.SetFlags(0)
+	fmt.Println("A0")
 	paths := getPaths()
+	fmt.Println("A1")
 	if len(paths) == 0 {
 		log.Fatalln("no rom files specified or found")
 	}
+	fmt.Println("A2")
 	ui.Run(paths)
 }
 
@@ -31,6 +34,7 @@ func getPaths() []string {
 	if err != nil {
 		return nil
 	}
+	fmt.Println("A3")
 	if info.IsDir() {
 		infos, err := ioutil.ReadDir(arg)
 		if err != nil {
